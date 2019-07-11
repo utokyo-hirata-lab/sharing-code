7883feb69a750045b1c16339b251a4b25043f69e
WataruTakahagi <watarut@outlook.com>
2019-07-11 21:20:43 +0900

    Update

--- get_from_web.py
+++ get_from_web.py
@@ -40,6 +40,7 @@ def call_temp(url): #URLから温度の情報をもってくる関数を定義
         if 'Temperature:' in inf: #Temperatureが存在する部分を抜き出す
             print(url,inf)
             return inf
+            break
 
 a = soup.find_all('a') #<a>タグで囲まれた部分の中身(JMC numberのURLが含まれる)を抜き出す
 url_list = []
@@ -54,21 +55,29 @@ for i in range(len(text)):
     if 'JCM number' in text[i]:
         name_list.append(text[i-1]) #賢いやり方かは怪しいが。
 
-with open('output.csv', 'a') as f: #'a'を指定すると追記できる (ファイルがない場合は新規)
+with open('output.csv', 'a', encoding='shift_jis') as f: #'a'を指定すると追記できる (ファイルがない場合は新規)
     k1 = 'Keyword'
     k2 = 'Speceis'
     k3 = 'JCM number'
     k4 = 'URL'
-    k5 =  'Information'
+    k5 = 'Temperature'
+    k6 = 'Information'
     filecheck = 0
-    with open('output.csv', 'r') as c:
+    with open('output.csv', 'r', encoding='shift_jis') as c:
         if len([i for i in c.readlines()]) > 0:
             filecheck = 1
-    fieldnames = [k1,k2,k3,k4,k5]
+    fieldnames = [k1,k2,k3,k4,k5,k6]
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     if filecheck == 0: writer.writeheader() #fileが空のときだけheaderを書き込む
     for line in url_list:
         ind = url_list.index(line)
-        print(Color.CYAN+'Calling... '+Color.GREEN+name_list[ind]+Color.END)
-        tmp = call_temp(line)
-        writer.writerow({k1:keyword,k2:name_list[ind],k3:line.split('=')[1],k4:line,k5:tmp})
+        info = call_temp(line)
+        spname = name_list[ind].replace('\xa0', '').replace('"','')
+        JCMnumber = line.split('=')[1]
+        tmp = float(info.split('°C')[0].split(':')[1])
+        try:
+            apx = ','.join(info.split(';')[1:]).replace('\xa0', '')
+        except IndexError:
+            apx = 'None.'
+        print(Color.CYAN+'Calling... '+Color.GREEN+spname+Color.END)
+        writer.writerow({k1:keyword,k2:spname,k3:JCMnumber,k4:line,k5:tmp,k6:apx}) #csvに書き込み
