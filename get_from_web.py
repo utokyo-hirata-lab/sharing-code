f4b8f433f3f12c633c901e7ba51b1bdd873a45bb
WataruTakahagi <watarut@outlook.com>
2019-07-11 16:39:30 +0900

    Update

--- get_from_web.py
+++ get_from_web.py
@@ -11,16 +11,23 @@ import urllib
 import csv
 import pprint
 
-#keyword = input('search key : ')
-keyword = 'hydrothermal'
+keyword = input('search key : ')
+#keyword = 'hydrothermal'
 url = 'https://www.jcm.riken.jp/cgi-bin/jcm/jcm_kojin?ANY=' + keyword
 parent = url.split('/')[0]+'//'+url.split('/')[2]
 html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, 'html.parser')
 
+def call_temp(url):
+    html = urllib.request.urlopen(url)
+    data = BeautifulSoup(html, 'html.parser')
+    text = data.get_text().splitlines()
+    for inf in text:
+        if 'Temperature:' in inf:
+            print(url,inf)
+            return inf
+
 a = soup.find_all("a")
-#for script in soup(["script", "style"]):
-#    script.decompose()
 url_list = []
 for tag in a:
     called = tag.get('href')
@@ -30,4 +37,5 @@ print(len(url_list))
 with open('output.csv', 'w') as f:
     writer = csv.writer(f)
     for line in url_list:
-        writer.writerow([line.split('=')[1],line])
+        tmp = call_temp(line)
+        writer.writerow([line.split('=')[1],line,tmp])
